03681e883cb2d7cc77c5f58946f574727a06de65
poorqwer0-tech <poorqwer0@gmail.com>
2026-08-20 14:03:20 +0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { StyleProvider } from './context/StyleContext';
+import { GlobalErrorBoundary } from './components/GlobalErrorBoundary';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <StyleProvider enableAntiFingerprint={true}>
+      <GlobalErrorBoundary>
+        <App />
+      </GlobalErrorBoundary>
+    </StyleProvider>
+  </React.StrictMode>
+);
